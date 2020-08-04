ccddf5f4048740e4e217700c8b05aa006fc17620
Rachit <rachitmathur8@gmail.com>
2020-08-05 05:06:16 +0530

    add chain-setup view

--- src/app/chain/chain-setup/chain-setup.component.ts
+++ src/app/chain/chain-setup/chain-setup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chain-setup',
   templateUrl: './chain-setup.component.html',
-  styleUrls: ['./chain-setup.component.css']
+  styleUrls: ['./chain-setup.component.scss']
 })
 export class ChainSetupComponent implements OnInit {
 
