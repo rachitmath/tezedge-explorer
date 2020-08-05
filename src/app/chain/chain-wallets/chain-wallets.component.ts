71d22663cbf4ee2279605151ad8ef4e1b4314083
Rachit <rachitmathur8@gmail.com>
2020-08-05 14:21:31 +0530

    add view in chain-wallet

--- src/app/chain/chain-wallets/chain-wallets.component.ts
+++ src/app/chain/chain-wallets/chain-wallets.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Wallet {
+  address: string;
+  balance: string;
+}
+
+const ELEMENT_DATA: Wallet[] = [
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' },
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' },
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' },
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' },
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' },
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' },
+  { address: 'tz1QJ1ACf4HnD6aHpZzex7CSUKr65y2xQNdH', balance: '1000 ꜩ' }
+
+];
+
 @Component({
   selector: 'app-chain-wallets',
   templateUrl: './chain-wallets.component.html',
-  styleUrls: ['./chain-wallets.component.css']
+  styleUrls: ['./chain-wallets.component.scss']
 })
 export class ChainWalletsComponent implements OnInit {
+
+  displayedColumns: string[] = ['Address', 'Balance'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
