088f4123854ac47a5bf95c590ec195b948549991
Rachit <rachitmathur8@gmail.com>
2020-08-05 16:10:36 +0530

    add view in chain-baking & chain-finish

--- src/app/chain/chain-finish/chain-finish.component.ts
+++ src/app/chain/chain-finish/chain-finish.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-chain-finish',
   templateUrl: './chain-finish.component.html',
-  styleUrls: ['./chain-finish.component.css']
+  styleUrls: ['./chain-finish.component.scss']
 })
 export class ChainFinishComponent implements OnInit {
+
+  public googleanalyticsSlideText = 'ENABLED';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onChangeChaneId(event: MatSlideToggleChange) {
+    if (event.checked) {
+      this.googleanalyticsSlideText = 'ENABLED';
+    } else {
+      this.googleanalyticsSlideText = 'DISABLED';
+    }
+  }
 }
